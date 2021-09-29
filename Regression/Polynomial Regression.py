46c160b0c8468882458fe9a050994dc4d67da3f9
Shrey Bodra <58246097+ShreyBodra@users.noreply.github.com>
2021-09-29 12:42:53 +0530

    .

--- Regression/Polynomial Regression.py
+++ Regression/Polynomial Regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
@@ -13,10 +14,11 @@ y = dataset.iloc[:, -1].values   # dependent variable
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # creating regression model
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X_train)
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
-
+regressor.fit(X_poly, y_train)
+y_pred = regressor.predict(poly_reg.transform(X_test))
 
 # testing accuracy
 accuracy = r2_score(y_test, y_pred)

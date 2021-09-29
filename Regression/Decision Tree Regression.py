13d093af7b6ee467b7f2ab6a47c33c3810a8bfdb
Shrey Bodra <58246097+ShreyBodra@users.noreply.github.com>
2021-09-29 12:53:07 +0530

    Update Decision Tree Regression.py

--- Regression/Decision Tree Regression.py
+++ Regression/Decision Tree Regression.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-
 from sklearn.metrics import r2_score
 
 # importing dataset

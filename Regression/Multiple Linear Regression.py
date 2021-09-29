7b0c54f4c9f2851922f283457fda184b1bb910a5
Shrey Bodra <58246097+ShreyBodra@users.noreply.github.com>
2021-09-29 12:25:44 +0530

    .

---
+++ Regression/Multiple Linear Regression.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+# importing dataset
+dataset = pd.read_excel('Folds5x2_pp.xlsx')
+
+X = dataset.iloc[:, :-1].values  # feature variable
+y = dataset.iloc[:, -1].values   # dependent variable
+
+# splitting into training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# creating regression model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+# testing accuracy
+r2_score(y_test, y_pred)
+print('Accuracy of the Model is :', r2_score)
